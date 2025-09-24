230b23f40ce6af239e4e946c66b034ac139d8abc
rupika-techpit <219994753+rupika-techpit@users.noreply.github.com>
2025-09-24 10:49:01 +0000

    Initial commit

---
+++ app/appSettings/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>App Settings</div>
+  )
+}
+
+export default page
---
+++ components/Layout/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 left-0 w-full bg-background border-t border-gray-200 dark:border-gray-700 py-2 shadow-md">
+      <p className="text-center text-xs text-foreground">
+        © 2025 My App. All rights reserved.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
+
+
+
