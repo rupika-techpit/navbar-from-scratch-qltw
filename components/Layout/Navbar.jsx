eb752c363b319ec8cc1431be145e4b58d4de0e6f
rupika-techpit <rupika@techpit.in>
2025-10-01 18:45:51 +0530

    wed2

--- components/Layout/Navbar.jsx
+++ components/Layout/Navbar.jsx
@@ -608,7 +608,7 @@ const Page = () => {
                                         setActive(item.name);
                                         setOpenDropdown(null);
                                       }}
-                                      className="flex items-center px-2 py-1 text-sm rounded hover:bg-[var(--color-primary-hover)]"
+                                      className="flex items-center px-2 py-1 text-sm rounded hover:bg-[var(--color-primary-hover)] hover:text-[var(--color-on-primary)]"
                                     >
                                       {sub.icon && (
                                         <sub.icon className="h-4 w-4 mr-2 shrink-0 z-50" />
@@ -645,8 +645,8 @@ const Page = () => {
                     }}
                     className={`inline-flex items-center px-3 py-2 text-sm font-medium rounded-md max-w-[120px] ${
                       active === item.name
-                        ? "bg-[var(--hover-bg)]"
-                        : "hover:bg-[var(--hover-bg)]"
+                        ? "bg-[var(--color-primary-hover)] text-[var(--color-on-primary)]"
+                        : "hover:bg-[var(--color-primary)] hover:text-[var(--color-on-primary)]"
                     }`}
                   >
                     <div className="overflow-hidden max-w-[80px]">
@@ -731,7 +731,7 @@ const Page = () => {
                             }}
                           >
                             {/* Row */}
-                            <div className="px-2 py-1 flex justify-between items-center hover:bg-[var(--hover-bg)] cursor-pointer">
+                            <div className="px-2 py-1 flex justify-between items-center hover:bg-[var(--color-primary-hover)] hover:text-[var(--color-on-primary)] cursor-pointer">
                               <div className="overflow-hidden max-w-[120px]">
                                 <span
                                   className={`inline-block whitespace-nowrap ${
